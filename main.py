56b298d603d802c276065cca2e6072ac936b137e
g4m34x <arteni.george.daniel@gmail.com>
2021-07-19 16:47:24 +0300

    added a custom logger to write state changes in a .log file

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+# #################################################
+# ########### Lib Import ##########################
+# #################################################
 from tkinter import *
 from pygame import mixer
 import threading,\
@@ -8,7 +11,148 @@ import threading,\
     tkinter.ttk, \
     urllib.request,\
     psutil,\
-    os
+    os,\
+    logging
+
+# #################################################
+# ########### Logger config #######################
+# #################################################
+
+# noinspection PyArgumentList
+logging.basicConfig(format='%(asctime)s,%(msecs)d %(levelname)-4s [%(filename)s:%(lineno)d -> %(funcName)s] ___ %(message)s',
+                    datefmt='%Y-%m-%d at %H:%M:%S',
+                    level=logging.INFO,
+                    handlers=[logging.FileHandler('pyTools_runtime.log',mode='a+'),
+                              logging.StreamHandler(stream=sys.stdout)])
+logger = logging.getLogger(__name__)
+
+# #################################################
+# ########### Methods Definition ##################
+# #################################################
+
+def resource_path(relative_path):
+    """ Get absolute path to resource, works for dev and for PyInstaller """
+    try:
+        # PyInstaller creates a temp folder and stores path in _MEIPASS
+        base_path = sys._MEIPASS
+    except Exception:
+        base_path = os.path.abspath(".")
+
+    return os.path.join(base_path, relative_path)
+
+def monitor_internet_conn_status():
+
+    relative_datetime = datetime.datetime.now()
+    conn_status = 'ONLINE'
+
+    while True:
+
+        if internet_conn_check_switch_variable.get() == 'on':
+
+            try:
+                urllib.request.urlopen('http://google.com') #Python 3.x
+                conn_status_new = 'ONLINE'
+            except:
+                conn_status_new = 'OFFLINE'
+
+            previous_state_time_sec = (datetime.datetime.now() - relative_datetime).seconds
+
+            if conn_status_new != conn_status:
+                conn_status = conn_status_new
+                relative_datetime = datetime.datetime.now()
+
+                if conn_status_new == 'ONLINE':
+                    mixer.init()
+                    mixer.music.load(os.path.join(resource_path('res'), "int_conn_online.mp3"))
+                    mixer.music.play()
+                    logger.info('Connection ONLINE. Was OFFLINE for {}'.format((str(datetime.timedelta(seconds=previous_state_time_sec)))))
+                elif conn_status_new == 'OFFLINE':
+                    mixer.init()
+                    mixer.music.load(os.path.join(resource_path('res'), "int_conn_offline.mp3"))
+                    mixer.music.play()
+                    logger.info('Connection OFFLINE. Was ONLINE for {}'.format((str(datetime.timedelta(seconds=previous_state_time_sec)))))
+
+            internet_conn_status_lbl.config(text='last event at {}\n{} for {}'.format(relative_datetime,
+                                                                                      conn_status_new,
+                                                                                      (str(datetime.timedelta(seconds=previous_state_time_sec)))))
+
+            time.sleep(10)
+        else:
+            time.sleep(10)
+
+def monitor_power_disconn_status():
+
+    relative_datetime = datetime.datetime.now()
+    power_status = 'AC-POWER'
+
+    while True:
+
+        if power_disconn_switch_variable.get() == 'on':
+
+            previous_state_time_sec = (datetime.datetime.now() - relative_datetime).seconds
+
+            power_status_new = 'AC-POWER' if psutil.sensors_battery().power_plugged else 'BATT-POWER'
+
+            if power_status_new != power_status:
+                power_status = power_status_new
+                relative_datetime = datetime.datetime.now()
+
+                if power_status_new == 'AC-POWER':
+                    mixer.init()
+                    mixer.music.load(os.path.join(resource_path('res'), "switched_to_ac.mp3"))
+                    mixer.music.play()
+                    logger.info('Currently using AC-POWER. BATTERY was used for {}'.format((str(datetime.timedelta(seconds=previous_state_time_sec)))))
+                elif power_status_new == 'BATT-POWER':
+                    mixer.init()
+                    mixer.music.load(os.path.join(resource_path('res'), "switched_to_batt.mp3"))
+                    mixer.music.play()
+                    logger.info('Currently using BATTERY. AC-POWER was used for {}'.format((str(datetime.timedelta(seconds=previous_state_time_sec)))))
+
+            power_disconn_lbl.config(text='last event at {}\n{} for {}'.format(relative_datetime,
+                                                                              power_status_new,
+                                                                              (str(datetime.timedelta(seconds=previous_state_time_sec)))))
+
+            time.sleep(10)
+        else:
+            time.sleep(10)
+
+def monitor_win_update_disabler():
+
+    T.config(state='normal')
+    T.insert('1.0', '{}: Delaying the startup by 10sec with a loop each 1h !\n'.format(str(datetime.datetime.now())))
+    T.config(state='disabled')
+    time.sleep(10)
+
+    while True:
+        if win_update_disabler_switch_variable.get() == 'on':
+            T.config(state='normal')
+            # output = os.system('cmd /c "sc stop "wuauserv""')
+            output = subprocess.call('sc stop "wuauserv"')
+            if output == 5:
+                T.insert('1.0', '{}: Run me with admin priviledges !\n'.format(str(datetime.datetime.now())))
+                logger.error('Cannot disable windows update. Run with admin priviledges.')
+            else:
+                output = subprocess.call('sc config "wuauserv" start=disabled')
+                if output != 0:
+                    T.insert('1.0', '{}: There was an issue changing the service startup type. Exit code {} !\n'.format(str(datetime.datetime.now()),
+                                                                                                                        output))
+                    logger.error('There was an issue changing the windows update service startup type.')
+                else:
+                    T.insert('1.0', '{}: windows update service disabled !\n'.format(str(datetime.datetime.now())))
+                    logger.info('Windows update service disabled.')
+            T.delete('100.0', '101.0')
+            T.config(state='disabled')
+            time.sleep(60*60)
+        else:
+            time.sleep(10)
+
+def on_closing():
+    window.destroy()
+    sys.exit()
+
+# #################################################
+# ################## Main Loop ####################
+# #################################################
 
 window = Tk()
 window.title("pyTools")
@@ -94,122 +238,12 @@ on_button = Radiobutton(window,
 off_button.grid(column=0, row=12)
 on_button.grid(column=1, row=12)
 
-def resource_path(relative_path):
-    """ Get absolute path to resource, works for dev and for PyInstaller """
-    try:
-        # PyInstaller creates a temp folder and stores path in _MEIPASS
-        base_path = sys._MEIPASS
-    except Exception:
-        base_path = os.path.abspath(".")
-
-    return os.path.join(base_path, relative_path)
-
-def monitor_internet_conn_status():
-
-    relative_datetime = datetime.datetime.now()
-    conn_status = 'ONLINE'
-
-    while True:
-
-        if internet_conn_check_switch_variable.get() == 'on':
-
-            try:
-                urllib.request.urlopen('http://google.com') #Python 3.x
-                conn_status_new = 'ONLINE'
-            except:
-                conn_status_new = 'OFFLINE'
-
-            if conn_status_new != conn_status:
-                conn_status = conn_status_new
-                relative_datetime = datetime.datetime.now()
-
-                if conn_status_new == 'ONLINE':
-                    mixer.init()
-                    mixer.music.load(os.path.join(resource_path('res'), "int_conn_online.mp3"))
-                    mixer.music.play()
-                    print('ONLINE connection')
-                elif conn_status_new == 'OFFLINE':
-                    mixer.init()
-                    mixer.music.load(os.path.join(resource_path('res'), "int_conn_offline.mp3"))
-                    mixer.music.play()
-                    print('OFFLINE connection')
-
-            internet_conn_status_lbl.config(text='last event at {}\n{} for {}'.format(relative_datetime,
-                                                                                      conn_status_new,
-                                                                                      (str(datetime.timedelta(seconds=(datetime.datetime.now() - relative_datetime).seconds)))))
-
-            time.sleep(10)
-        else:
-            time.sleep(10)
-
-def monitor_power_disconn_status():
-
-    relative_datetime = datetime.datetime.now()
-    power_status = 'AC-POWER'
-
-    while True:
-
-        if power_disconn_switch_variable.get() == 'on':
-
-            power_status_new = 'AC-POWER' if psutil.sensors_battery().power_plugged else 'BATT-POWER'
-
-            if power_status_new != power_status:
-                power_status = power_status_new
-                relative_datetime = datetime.datetime.now()
-
-                if power_status_new == 'AC-POWER':
-                    mixer.init()
-                    mixer.music.load(os.path.join(resource_path('res'), "switched_to_ac.mp3"))
-                    mixer.music.play()
-                    print('AC power')
-                elif power_status_new == 'BATT-POWER':
-                    mixer.init()
-                    mixer.music.load(os.path.join(resource_path('res'), "switched_to_batt.mp3"))
-                    mixer.music.play()
-                    print('BATTERY power')
-
-            power_disconn_lbl.config(text='last event at {}\n{} for {}'.format(relative_datetime,
-                                                                              power_status_new,
-                                                                              (str(datetime.timedelta(seconds=(datetime.datetime.now() - relative_datetime).seconds)))))
-
-            time.sleep(10)
-        else:
-            time.sleep(10)
-
-def monitor_win_update_disabler():
-
-    T.config(state='normal')
-    T.insert('1.0', '{}: Delaying the startup by 10sec with a loop each 1h !\n'.format(str(datetime.datetime.now())))
-    T.config(state='disabled')
-    time.sleep(10)
-
-    while True:
-        if win_update_disabler_switch_variable.get() == 'on':
-            T.config(state='normal')
-            # output = os.system('cmd /c "sc stop "wuauserv""')
-            output = subprocess.call('sc stop "wuauserv"')
-            if output == 5:
-                T.insert('1.0', '{}: Run me with admin priviledges !\n'.format(str(datetime.datetime.now())))
-            else:
-                output = subprocess.call('sc config "wuauserv" start=disabled')
-                if output != 0:
-                    T.insert('1.0', '{}: There was an issue changing the service startup type. Exit code {} !\n'.format(str(datetime.datetime.now()),
-                                                                                                                        output))
-                else:
-                    T.insert('1.0', '{}: windows update service disabled !\n'.format(str(datetime.datetime.now())))
-            T.delete('100.0', '101.0')
-            T.config(state='disabled')
-            time.sleep(60*60)
-        else:
-            time.sleep(10)
-
 threading.Thread(target=monitor_win_update_disabler, args=(), daemon=True).start()
 threading.Thread(target=monitor_internet_conn_status, args=(), daemon=True).start()
 threading.Thread(target=monitor_power_disconn_status, args=(), daemon=True).start()
 
-def on_closing():
-    window.destroy()
-    sys.exit()
-
 window.protocol("WM_DELETE_WINDOW", on_closing)
+
+logger.info('############## pyTools initialized ##############')
+
 window.mainloop()
